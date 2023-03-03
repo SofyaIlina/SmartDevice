38ef9d31549b50402fbe97aaf7dc456f9502caae
SofyaIlina <kenny8556@yandex.ru>
2023-03-03 19:04:35 +0500

    переполнение шапки, смена цвета шапки при скролле

--- source/js/modules/header-scroll.js
+++ source/js/modules/header-scroll.js
@@ -1,4 +1,11 @@
 export const headerScroll = () =>
   document.addEventListener('scroll', () => {
-    document.querySelector('.main-header').style.background = 'green';
-})
+    let top = window.pageYOffset;
+    let element = document.querySelector('.main-header');
+    element.style.transition = '0.8s ease';
+    if (top >= 150) {
+      element.style.background = 'linear-gradient(180deg, #012478 84.9%, rgba(4, 24, 39, 0) 100%)';
+    } else if (top <= 200) {
+      element.style.background = 'linear-gradient(180deg, #041827 84.9%, rgba(4, 24, 39, 0) 100%)';
+    }
+  });
