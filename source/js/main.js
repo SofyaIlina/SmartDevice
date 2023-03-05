0c67be8a1bad6b9efef4f72d4251f69f34717ee8
SofyaIlina <kenny8556@yandex.ru>
2023-03-06 01:03:23 +0500

    футер и промо блок

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,7 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
 import {Form} from './modules/form-validate/form';
-import { headerScroll } from './modules/header-scroll';
+import {headerScroll} from './modules/header-scroll';
 
 // ---------------------------------
 
